d771ed20d8c49bceacf46ede5f5091293c8b1077
Orandifelix <125347734+Orandifelix@users.noreply.github.com>
2023-04-11 17:27:36 +0300

    second commit

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ function addingEventListener() {
     let input = document.getElementById('input');
     input.addEventListener('click', function() {
     alert('I was clicked!');
-    })
+    });
 }
-
+addingEventListener();
 
